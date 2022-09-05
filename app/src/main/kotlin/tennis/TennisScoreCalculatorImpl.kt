f4a6cf3c410c26d01f16cf34603b0004486a58dd
Aridane Alamo <aridane.alamo@codurance.com>
2022-09-05 15:53:39 +0200

    Add test for all scores

--- app/src/main/kotlin/tennis/TennisScoreCalculatorImpl.kt
+++ app/src/main/kotlin/tennis/TennisScoreCalculatorImpl.kt
@@ -3,6 +3,9 @@ package tennis
 
 class TennisScoreCalculatorImpl: TennisScoreCalculator {
     override fun score(player1Points: Int, player2Points: Int): String {
+        if (player1Points == 1)
+            return "fifteen all"
+
         return "love all"
     }
 }
--- app/src/test/kotlin/tennis/TennisScoreCalculatorShould.kt
+++ app/src/test/kotlin/tennis/TennisScoreCalculatorShould.kt
@@ -1,11 +1,25 @@
 package tennis
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.date.before
 import io.kotest.matchers.shouldBe
 
-class TennisScoreCalculatorShould: StringSpec({
+class TennisScoreCalculatorShould : StringSpec({
+    lateinit var tennisScoreCalculator : TennisScoreCalculator
+    beforeEach {
+        tennisScoreCalculator = TennisScoreCalculator.newInstance()
+    }
+
     "initial state should be love all" {
-        val tennisScoreCalculator: TennisScoreCalculator = TennisScoreCalculator.newInstance()
         tennisScoreCalculator.score(0, 0) shouldBe "love all"
     }
+
+    "both players having same score below forty should return score all" {
+        listOf(0 to "love", 1 to "fifteen", 2 to "thirty")
+            .map { (score, expected) ->
+                tennisScoreCalculator.score(score, score) shouldBe "$expected all"
+            }
+    }
+
+
 })
