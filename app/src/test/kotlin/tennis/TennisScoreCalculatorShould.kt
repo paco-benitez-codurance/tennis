9ab8eec3a2bb5f1c3a0f96f36e4262c0cf4afb93
Paco Benitez <paco.benitez@sfd.gov.sa>
2022-09-05 16:57:09 +0200

    Change test style to free test

--- app/src/test/kotlin/tennis/TennisScoreCalculatorShould.kt
+++ app/src/test/kotlin/tennis/TennisScoreCalculatorShould.kt
@@ -1,45 +1,53 @@
 package tennis
 
 import io.kotest.assertions.throwables.shouldThrow
-import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
 
-class TennisScoreCalculatorShould : StringSpec({
+class TennisScoreCalculatorShould : FreeSpec({
     lateinit var tennisScoreCalculator: TennisScoreCalculator
     lateinit var tennisScoreNumbers: TennisScoreNumbers
 
-    beforeEach {
+    beforeContainer {
         tennisScoreNumbers = mockk()
         tennisScoreCalculator = TennisScoreCalculator.newInstance(tennisScoreNumbers)
     }
 
-    "should not allow negative numbers" {
-        every { tennisScoreNumbers.isEven(any(), any()) } returns false
-        shouldThrow<NotValidPointException> {
-            tennisScoreCalculator.score(-1, 0)
+    "Invalid Inputs" - {
+        "should not allow negative numbers" {
+            every { tennisScoreNumbers.isEven(any(), any()) } returns false
+            shouldThrow<NotValidPointException> {
+                tennisScoreCalculator.score(-1, 0)
+            }
         }
     }
 
-    "return score all when both players having same score below forty" {
+    "return score all when both players having same score below forty" - {
         every { tennisScoreNumbers.isEven(any(), any()) } returns true
         listOf(0 to "love", 1 to "fifteen", 2 to "thirty")
-            .map { (score, expected) ->
-                tennisScoreCalculator.score(score, score) shouldBe "$expected all"
+            .forEach { (score, expected) ->
+                "checking score $score" {
+                    tennisScoreCalculator.score(score, score) shouldBe "$expected all"
+                }
             }
     }
 
-    "return human readable scores when player1 have score distinct below forty and over 0 and player2 have score 0" {
+    "return human readable scores when player1 have score distinct below forty and over 0 and player2 have score 0" - {
         every { tennisScoreNumbers.isEven(any(), any()) } returns false
         listOf(
             (1 to 0) to "fifteen to love",
             (2 to 0) to "thirty to love",
             (3 to 0) to "forty to love",
-        ).map { (scores, expected) -> tennisScoreCalculator.score(scores.first, scores.second) shouldBe expected }
+        ).forEach { (scores, expected) ->
+            "$scores should be $expected" {
+                tennisScoreCalculator.score(scores.first, scores.second) shouldBe expected
+            }
+        }
     }
 
-    "method call should be symmetric" {
+    "method call should be symmetric" - {
         every { tennisScoreNumbers.isEven(any(), any()) } returns false
 
         fun reverseOrderOfWords(s: String) = s.split(" ").reversed().joinToString(" ")
@@ -48,11 +56,13 @@ class TennisScoreCalculatorShould : StringSpec({
             (1 to 0),
             (2 to 0),
             (3 to 0)
-        ).map { (player1Points, player2Points) ->
-            tennisScoreCalculator.score(
-                player1Points,
-                player2Points
-            ) shouldBe reverseOrderOfWords(tennisScoreCalculator.score(player2Points, player1Points))
+        ).forEach { (player1Points, player2Points) ->
+            "$player1Points vs $player2Points should be symmetric" {
+                tennisScoreCalculator.score(
+                    player1Points,
+                    player2Points
+                ) shouldBe reverseOrderOfWords(tennisScoreCalculator.score(player2Points, player1Points))
+            }
         }
 
     }
