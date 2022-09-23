8dfac2097059badfe786371e05a8b059e91b41e1
Paco Benitez <paco.benitez@sfd.gov.sa>
2022-09-23 16:30:12 +0200

    format

--- app/src/main/kotlin/tennis/TennisScoreCalculatorImpl.kt
+++ app/src/main/kotlin/tennis/TennisScoreCalculatorImpl.kt
@@ -3,13 +3,13 @@ package tennis
 
 class TennisScoreCalculatorImpl(private val tennisScoreNumbers: TennisScoreNumbers) : TennisScoreCalculator {
     override fun score(player1Points: Int, player2Points: Int): String {
-        return if (tennisScoreNumbers.isDeuce(player1Points,player2Points))
+        return if (tennisScoreNumbers.isDeuce(player1Points, player2Points))
             "deuce"
-        else if (tennisScoreNumbers.isEven(player1Points,player2Points))
+        else if (tennisScoreNumbers.isEven(player1Points, player2Points))
             humanPoint(player1Points) + " all"
-        else if (tennisScoreNumbers.isAdvantagePlayer1(player1Points,player2Points))
+        else if (tennisScoreNumbers.isAdvantagePlayer1(player1Points, player2Points))
             "advantage player1"
-        else if (tennisScoreNumbers.isAdvantagePlayer2(player1Points,player2Points))
+        else if (tennisScoreNumbers.isAdvantagePlayer2(player1Points, player2Points))
             "advantage player2"
         else
             "${humanPoint(player1Points)} to ${humanPoint(player2Points)}"
